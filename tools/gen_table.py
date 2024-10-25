62d7257aaaa6333aedf06ee0f652b66293c9eac2
Raven Penfold <110283510+ravenPenfold@users.noreply.github.com>
2024-10-25 15:34:22 +0100

    Delete tools/gen_table.py

--- tools/gen_table.py
+++
@@ -1,7 +0,0 @@
-# Let's create definitions that people can change easily
-soundInclude = "../gs_sound_data.sbdl"
-pvPrefix = "SEQ_PV_"
-bgmPrefix = "SEQ_GS_"
-mePrefix = "SEQ_ME_"
-sePrefix = "SEQ_SE_"
-outputFile = "../docs/soundlist.html"
